9fed16f14fd49f899bb7ad77d84573096e440ffd
Nguyễn Thanh Hoàng <adminwmt@gmail.com>
2015-05-24 17:39:50 +0700

    Create admin.menu.php

---
+++ nv4_module_diemex/modules/diemex/admin.menu.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author Webvang (Hoang.nguyen@webvang.vn)
+ * @Copyright (C) 2015 Webvang. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate 23/05/2015 10:27
+ */
+
+if( ! defined( 'NV_ADMIN' ) ) die( 'Stop!!!' );
+
+    $submenu['notice'] = $lang_module['notice'];
+	$allow_func = array( 'main', 'notice');
+
+	
