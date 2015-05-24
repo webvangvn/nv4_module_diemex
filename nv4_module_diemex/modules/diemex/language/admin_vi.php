d9b9ff1ff10388e9f50de13aa2127e96477990e1
Nguyễn Thanh Hoàng <adminwmt@gmail.com>
2015-05-24 17:43:30 +0700

    Create admin_vi.php

---
+++ nv4_module_diemex/modules/diemex/language/admin_vi.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author hongoctrien (01692777913@yahoo.com)
+ * @Copyright (C) 2012 by hongoctrien
+ * @Update to 4.x webvang (hoang.nguyen@webvang.vn)
+ * @Language Tiếng việt
+ * @Createdate July 05, 2012 10:47:41 AM
+ */
+
+ if (! defined('NV_ADMIN') or ! defined('NV_MAINFILE')){
+ die('Stop!!!');
+}
+
+$lang_translator['author'] ="VINADES.,JSC (contact@vinades.vn)";
+$lang_translator['createdate'] ="04/03/2010, 15:22";
+$lang_translator['copyright'] ="@Copyright (C) 2010 VINADES.,JSC. All rights reserved";
+$lang_translator['info'] ="";
+$lang_translator['langtype'] ="lang_module";
+
+$lang_module['config'] = "Cấu hình";
+$lang_module['notice'] = "Thông báo";
+$lang_module['error_csdl'] = "Có lỗi xảy ra!";
+$lang_module['ok_csdl'] = "Chèn dữ liệu thành công!";
+$lang_module['save'] = "Lưu";
+$lang_module['namhoc'] = "Năm học";
+$lang_module['tieude'] = "Tiêu đề";
+$lang_module['banghi'] = "Tổng số bản ghi hiện có: ";
+$lang_module['not_delete'] = "Không xóa được dữ liệu";
+$lang_module['deleted'] = "Xóa dữ liệu thành công";
+$lang_module['deleted_conf'] = "Bạn có chắc chắn muốn xóa hết dữ liệu?";
+$lang_module['delete'] = "Xóa hết";
+
