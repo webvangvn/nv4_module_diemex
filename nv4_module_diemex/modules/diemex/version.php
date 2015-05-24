5cc05f8aec0bb61fc9f97b58ce54f148d59258f3
Nguyễn Thanh Hoàng <adminwmt@gmail.com>
2015-05-24 17:34:27 +0700

    Create version.php

---
+++ nv4_module_diemex/modules/diemex/version.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author hongoctrien (01692777913@yahoo.com)
+ * @Update to 4.x webvang (hoang.nguyen@webvang.vn)
+ * @Copyright (C) 2012 by hongoctrien
+ * @Createdate July 05, 2012 10:47:41 AM
+ */
+
+if( ! defined( 'NV_ADMIN' ) or ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+
+$module_version = array(
+	"name" => "Diemex", //
+	"modfuncs" => "main, print", //
+	"is_sysmod" => 0, //
+	"virtual" => 0, //
+	"version" => "4.0.00", //
+	"date" => "Sun, 22 July 2012 00:00:00 GMT", //
+	"author" => "hongoctrien (01692777913@yahoo.com),update to 4x by webvang ", //
+	"note" => "",
+	"uploads_dir" => array( $module_name )
+);
+
