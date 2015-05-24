93955926b1187e085d7e253733d898ddb313455a
Nguyễn Thanh Hoàng <adminwmt@gmail.com>
2015-05-24 17:53:30 +0700

    Create vi.php

---
+++ nv4_module_diemex/modules/diemex/language/vi.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @Project NUKEVIET 3.4
+ * @Author hongoctrien (01692777913@yahoo.com)
+ * @Update to 4.x webvang (hoang.nguyen@webvang.vn)
+ * @Copyright (C) 2012 by hongoctrien
+ * @Language Tiếng việt
+ * @Createdate July 05, 2012 10:47:41 AM
+ */
+
+ if (!defined( 'NV_MAINFILE' )) {
+ die('Stop!!!');
+}
+
+$lang_translator['author'] ="hongoctrien (01692777913@yahoo.com)";
+$lang_translator['createdate'] ="04/07/2012, 11:22";
+$lang_translator['copyright'] ="@Copyright (C) 2012 2mit.org";
+$lang_translator['info'] ="";
+$lang_translator['langtype'] ="lang_module";
+
+$lang_module['config'] = "Cấu hình";
+$lang_module['search'] = "Tìm kiếm";
+$lang_module['error'] = "Dữ liệu bạn nhập vào không đúng quy tắc hoặc không tồn tại<br />Vui lòng kiểm tra lại thông tin!";
+$lang_module['title'] = "OK";
+
+$lang_module['sbd'] = "SBD";
+$lang_module['ho'] = "Họ";
+$lang_module['ten'] = "Tên";
+$lang_module['lop'] = "Lớp";
+$lang_module['ngsinh'] = "Ngày sinh";
+$lang_module['phong'] = "Phòng";
+$lang_module['toan'] = "Toán";
+$lang_module['ly'] = "Lý";
+$lang_module['hoa'] = "Hóa";
+$lang_module['sinh'] = "Sinh";
+$lang_module['van'] = "Văn";
+$lang_module['su'] = "Sử";
+$lang_module['dia'] = "Địa";
+$lang_module['anh'] = "Anh";
+$lang_module['gd'] = "Tổng";
+
+$lang_module['f1'] = "Điểm thi của học sinh ";
+$lang_module['f2'] = "Tìm thấy %1\$s kết quả với từ khóa \"%2\$s\"";
+$lang_module['trungten'] = "Xem những người có cùng tên với ";
+$lang_module['trunglop'] = "Xem những người học chung lớp với ";
+$lang_module['trungp'] = "Xem những người thi cùng phòng với ";
+$lang_module['ht_lop'] = "Tổng hợp kết quả của lớp ";
+$lang_module['ht_phong'] = "Tổng hợp kết quả của phòng ";
+
+$lang_module['print'] = "&nbsp;&nbsp;In kết quả";
+?>
